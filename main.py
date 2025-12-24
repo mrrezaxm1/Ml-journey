b63eb8ebbff2dd6269a95fa38bbff200a0d816e0
mrrezaxm1 <rezaxxalipoor@gmail.com>
2025-12-24 22:56:59 +0330

    Implement data analysis in main.py
    Day 1 ## 
    
    Created project structure - 
    Practiced basic Python scripting - 
    Started improving technical English -

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello worlled')
+import numpy as np 
+import pandas as pd
+
+def main():
+  data = {
+    'age' : [25,32,47,51],
+    'salary' : [40000,52000,80000,110000]
+  }
+  df = pd.DataFrame(data)
+  print('mean age' , np.mean(df['age']))
+  print('mean salary ' , np.mean(df['salary']))
+
+if __name__== '__main__':
+  main()
